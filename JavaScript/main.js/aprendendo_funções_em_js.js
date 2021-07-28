eeae7b2ce854f73db0d535ce072c9f7bff200119
root <root@acertofin.ODDBN.LOCAL>
2021-07-28 20:11:20 -0300

    adicionando novos exercícios  em JS

---
+++ JavaScript/main.js/aprendendo_funções_em_js.js
@@ -0,0 +1,12 @@
+//esse algorítimo exemplifica uma função de soma em JS
+function soma(n1, n2){
+	return n1 + n2;
+}
+console.log(soma(5,10));
+
+//esse algoritimo exemplifica uma função de troca de frases (replace)
+function setReplace(frase, nome, novo_nome ){
+	return frase.replace(nome, novo_nome)
+}
+console.log(setReplace("Vai japão !!!", "japão", "Brasil"));
+
---
+++ JavaScript/main.js/função_com_if_e_else.js
@@ -0,0 +1,13 @@
+//esse script realiza uma função com if e else
+var validar = 0;
+function validaIdade(idade){
+	if (idade >= 18){
+		validar = alert("você é maior de idade");
+	}else{
+		validar = alert("você é menor de idade");
+	}
+	return validar;
+}
+var idade = prompt("Qual sua idade ? ");
+validaIdade(idade)
+alert(validar);
